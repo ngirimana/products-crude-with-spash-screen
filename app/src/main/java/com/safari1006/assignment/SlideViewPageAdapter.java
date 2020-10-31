b697c329de973d1e5b1eb4c046f897537416bbac
ngirimana <chadrackngirimana@gmail.com>
2020-10-31 19:00:42 +0200

    Fastfood splash screen and login

---
+++ app/src/main/java/com/safari1006/assignment/SlideViewPageAdapter.java
@@ -0,0 +1,112 @@
+package com.safari1006.assignment;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+public class SlideViewPageAdapter extends PagerAdapter {
+    Context ctx;
+
+    public SlideViewPageAdapter(Context ctx) {
+        this.ctx = ctx;
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view==object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        LayoutInflater layoutInflater= (LayoutInflater) ctx.getSystemService(ctx.LAYOUT_INFLATER_SERVICE);
+        View view=layoutInflater.inflate(R.layout.slide_screen,container,false);
+        ImageView logo=view.findViewById(R.id.logo);
+
+        ImageView ind1=view.findViewById(R.id.ind1);
+        ImageView ind2=view.findViewById(R.id.ind2);
+        ImageView ind3=view.findViewById(R.id.ind3);
+
+        TextView title=view.findViewById(R.id.title);
+        TextView desc=view.findViewById(R.id.desc);
+        ImageView next=view.findViewById(R.id.next);
+        ImageView back=view.findViewById(R.id.back);
+        Button btnGetStarted=view.findViewById(R.id.btnGetStart);
+        btnGetStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(ctx,MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK| Intent.FLAG_ACTIVITY_NEW_TASK);
+                ctx.startActivity(intent);
+            }
+
+        });
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SlideActivity.viewPager.setCurrentItem(position+1);
+            }
+        });
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SlideActivity.viewPager.setCurrentItem(position-1);
+            }
+        });
+        switch (position){
+            case 0:
+                logo.setImageResource(R.drawable.img1);
+                ind2.setImageResource(R.drawable.selected);
+                ind1.setImageResource(R.drawable.unselected);
+                ind3.setImageResource(R.drawable.unselected);
+                title.setText("Fast Food");
+                desc.setText("The shopping mall has architecture the schools and hospitals can only dream of");
+                next.setVisibility(View.VISIBLE);
+                back.setVisibility(View.GONE);
+
+                break;
+            case 1:
+                logo.setImageResource(R.drawable.img2);
+                ind1.setImageResource(R.drawable.selected);
+                ind2.setImageResource(R.drawable.unselected);
+                ind3.setImageResource(R.drawable.unselected);
+                title.setText("Fast Delivery");
+                desc.setText("Fast delivery than another on else");
+                back.setVisibility(View.VISIBLE);
+                next.setVisibility(View.VISIBLE);
+                break;
+            case 2:
+                logo.setImageResource(R.drawable.img3);
+                ind1.setImageResource(R.drawable.unselected);
+                ind2.setImageResource(R.drawable.unselected);
+                ind3.setImageResource(R.drawable.selected);
+                title.setText("Easy Payment");
+                desc.setText("Pay with your convenient method of your choice ");
+                back.setVisibility(View.VISIBLE);
+                next.setVisibility(View.GONE);
+                break;
+        }
+
+        container.addView(view);
+        return view;
+
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View) object);
+    }
+}
