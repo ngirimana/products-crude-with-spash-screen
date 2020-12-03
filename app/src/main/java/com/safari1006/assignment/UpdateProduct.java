f4822f5f5a6346e8deea2189869b8509d001460a
ngirimana <chadrackngirimana@gmail.com>
2020-12-03 10:19:03 +0200

    delete item

--- app/src/main/java/com/safari1006/assignment/UpdateProduct.java
+++ app/src/main/java/com/safari1006/assignment/UpdateProduct.java
@@ -12,54 +12,61 @@ import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
-import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 
-public class AddProduct extends AppCompatActivity {
-    private EditText prodName,prodDesc,prodPrice,prodExpiry,imageLink;
-    private Button btnAddProd;
-     String url="https://product-crud-op.herokuapp.com/api/v1/products/add-product";
+public class UpdateProduct extends AppCompatActivity {
+    private EditText prodName,prodDesc,prodPrice,imageLink;
+    private Button btnUpdateProd;
+    private String id,name,price,desc,image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_product);
-
+        setContentView(R.layout.update_product);
         prodName=findViewById(R.id.productName);
         prodDesc=findViewById(R.id.productDesc);
         prodPrice=findViewById(R.id.productPrice);
-        btnAddProd= findViewById(R.id.btnAddProd);
+        btnUpdateProd= findViewById(R.id.btnUpdateProd);
         imageLink=findViewById(R.id.productImageLink);
 
-        String name =prodName.getText().toString();
-        String description =""+prodDesc.getText().toString();
-        String price =""+prodPrice.getText().toString();
+      Intent intent=getIntent();
+        id=intent.getStringExtra("ID");
+       name= intent.getStringExtra("NAME");
+       price= intent.getStringExtra("PRICE");
+        desc=intent.getStringExtra("DESCRIPTION");
+        image=intent.getStringExtra("IMAGE");
 
+        prodName.setText(name);
+        prodDesc.setText(desc);
+        prodPrice.setText(price);
+        imageLink.setText(image);
 
-        btnAddProd.setOnClickListener(new View.OnClickListener() {
+        btnUpdateProd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                addProduct(
+            updateProduct(
+                    ""+id,
                     ""+prodName.getText().toString(),
                     ""+prodPrice.getText().toString(),
                     ""+prodDesc.getText().toString(),
-                        ""+imageLink.getText().toString()
-                        );}
+                    ""+imageLink.getText().toString()
+            );
+            }
         });
+
     }
 
-    private void addProduct(String name, String price, String description, String imageLink) {
+    private void updateProduct(String id, String name, String price, String description, String imageLink) {
+        String url="https://product-crud-op.herokuapp.com/api/v1/products/all/"+id;
 
         final JSONObject jsonObject = new JSONObject();
         try {
@@ -72,13 +79,13 @@ public class AddProduct extends AppCompatActivity {
             e.printStackTrace();
         }
         //build response
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, jsonObject, new Response.Listener<JSONObject>() {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.PATCH, url, jsonObject, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
                 try {
-
-                    Toast.makeText(AddProduct.this, "Added Successfully", Toast.LENGTH_SHORT).show();
-                    Intent intent= new Intent(AddProduct.this,AllProduct.class);
+                        System.out.println(response);
+                    Toast.makeText(UpdateProduct.this, "Updated Successfully", Toast.LENGTH_SHORT).show();
+                    Intent intent= new Intent(UpdateProduct.this,AllProduct.class);
                     startActivity(intent);
 
                 } catch (Exception e) {
@@ -89,7 +96,7 @@ public class AddProduct extends AppCompatActivity {
             @Override
             public void onErrorResponse(VolleyError error) {
                 System.out.println(jsonObject);
-                Toast.makeText(AddProduct.this, (CharSequence) error, Toast.LENGTH_SHORT).show();
+                Toast.makeText(UpdateProduct.this, (CharSequence) error, Toast.LENGTH_SHORT).show();
             }
         }) {
             @NonNull
@@ -103,4 +110,7 @@ public class AddProduct extends AppCompatActivity {
         };
         Volley.newRequestQueue(this).add(jsonObjectRequest);
     }
-}
+
+
+    }
+
